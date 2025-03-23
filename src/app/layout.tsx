90a94c821593254f4aee77b932e510d082ca569b
Gibsondignity <gibsondignty@gmail.com>
2025-03-23 03:06:55 +0000

    layout updated to include footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 
 const poppins = Poppins({ 
@@ -31,7 +32,7 @@ export default function RootLayout({
           <Header />
         {/* Header */}
         { children }
-        {/* Footer */}
+        <Footer />
         </main>
       </body>
     </html>
